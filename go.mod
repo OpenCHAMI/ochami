b4d2a0bbeef62f606aede822859ee3550da9bfb5
Devon Bautista <devnb@devnb.net>
2024-12-07 15:07:04 -0700

    WIP: refactor: replace viper with koanf

--- go.mod
+++ go.mod
@@ -6,7 +6,12 @@ require (
 	github.com/OpenCHAMI/bss v1.31.3
 	github.com/OpenCHAMI/cloud-init v0.1.1
 	github.com/OpenCHAMI/smd/v2 v2.16.1
+	github.com/go-viper/mapstructure/v2 v2.2.1
 	github.com/google/uuid v1.6.0
+	github.com/knadh/koanf/parsers/yaml v0.1.0
+	github.com/knadh/koanf/providers/file v1.1.2
+	github.com/knadh/koanf/providers/structs v0.1.0
+	github.com/knadh/koanf/v2 v2.1.2
 	github.com/lestrrat-go/jwx v1.2.30
 	github.com/openchami/schemas v0.0.0-20240826142248-37b8af32208a
 	github.com/rs/zerolog v1.33.0
@@ -23,6 +28,7 @@ require (
 	github.com/buger/jsonparser v1.1.1 // indirect
 	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.3.0 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-jose/go-jose/v4 v4.0.4 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
@@ -38,6 +44,7 @@ require (
 	github.com/hashicorp/vault/api v1.14.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/invopop/jsonschema v0.12.0 // indirect
+	github.com/knadh/koanf/maps v0.1.1 // indirect
 	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
@@ -47,8 +54,10 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,90 @@
 package config
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"os/user"
 	"path/filepath"
-	"strings"
 
 	"github.com/OpenCHAMI/ochami/internal/log"
-	"github.com/spf13/viper"
+	"github.com/go-viper/mapstructure/v2"
+	kyaml "github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+	"github.com/knadh/koanf/v2"
+	"gopkg.in/yaml.v3"
 )
+
+// Config represents the structure of a configuration file.
+type Config struct {
+	Log            ConfigLog       `yaml:"log,omitempty"`
+	DefaultCluster string          `yaml:"default-cluster,omitempty"`
+	Clusters       []ConfigCluster `yaml:"clusters,omitempty"`
+}
+
+type ConfigLog struct {
+	Format string `yaml:"format,omitempty"`
+	Level  string `yaml:"level,omitempty"`
+}
+
+type ConfigCluster struct {
+	Name    string              `yaml:"name,omitempty"`
+	Cluster ConfigClusterConfig `yaml:"cluster,omitempty"`
+}
+
+type ConfigClusterConfig struct {
+	BaseURI string `yaml:"base-uri,omitempty"`
+}
+
+const ProgName = "ochami"
+
+// Default configuration values if either no configuration files exist or the
+// configuration files don't contain values for items that need them.
+var DefaultConfig = Config{
+	Log: ConfigLog{
+		Format: "json",
+		Level:  "warning",
+	},
+}
+
+var (
+	GlobalConfig = DefaultConfig // Global config struct
+	GlobalKoanf  *koanf.Koanf    // Koanf instance for gobal config struct
+
+	// Since logging isn't set up until after config is read, this variable
+	// allows more verbose printing if true for more verbose logging
+	// pre-config parsing.
+	EarlyVerbose bool
+
+	configParser = kyaml.Parser() // Koanf YAML parser provider
+
+	// Global koanf struct configuration
+	kConfig = koanf.Conf{Delim: ".", StrictMerge: true}
+
+	// koanf unmarshal config used in unmarshalling function
+	kUnmarshalConf = koanf.UnmarshalConf{
+		Tag: "yaml", // Tag for determining mapping to struct members
+		DecoderConfig: &mapstructure.DecoderConfig{
+			ErrorUnused: true,          // Err if unknown keys found
+			Result:      &GlobalConfig, // Unmarshal to global config
+		},
+	}
+)
+
+func earlyLog(arg ...interface{}) {
+	if EarlyVerbose {
+		fmt.Fprintf(os.Stderr, "%s: ", ProgName)
+		fmt.Fprintln(os.Stderr, arg...)
+	}
+}
+
+func earlyLogf(fstr string, arg ...interface{}) {
+	if EarlyVerbose {
+		fmt.Fprintf(os.Stderr, "%s: ", ProgName)
+		fmt.Fprintf(os.Stderr, fstr+"\n", arg...)
+	}
+}
 
 // RemoveFromSlice removes an element from a slice and returns the resulting
 // slice. The element to be removed is identified by its index in the slice.
@@ -23,44 +100,105 @@ func RemoveFromSlice[T any](slice []T, index int) []T {
 // configuration format. If format is empty, the format is guessed by the config
 // file's file extension. If there is no file extension or format is empty, YAML
 // format is used.
-func LoadConfig(path, format string) error {
-	if path == "" {
-		return fmt.Errorf("no configuration file path passed")
-	}
-	base := filepath.Dir(path)
-	name := filepath.Base(path)
-	ext := strings.Trim(filepath.Ext(path), ".")
-	fullPath := filepath.Join(base, name)
-	log.Logger.Debug().Msgf("Configuration file is: %s", fullPath)
+func LoadConfig(path string) error {
+	earlyLog("early verbose log messages activated")
 
-	// Determine format of config file to tell viper to use.
-	var viperFormat string
-	if format != "" {
-		if ext != "" {
-			log.Logger.Debug().Msgf("Using passed value of %q as config file format even though file extension is %q", format, ext)
-		} else {
-			log.Logger.Debug().Msgf("Using passed value of %q as config file format", format)
+	// Initialize global koanf structure
+	GlobalKoanf = koanf.NewWithConf(kConfig)
+
+	// If a config file was specified, load it alone. Do not try to merge
+	// its config with any other configuration.
+	if path != "" {
+		earlyLogf("using passed config file %s", path)
+		earlyLogf("parsing %s", path)
+		if err := GlobalKoanf.Load(file.Provider(path), configParser); err != nil {
+			return fmt.Errorf("failed to load specified config file %s: %w", path, err)
 		}
-		viperFormat = strings.ToLower(format)
-	} else {
-		if ext != "" {
-			log.Logger.Debug().Msgf("No config file format passed, inferring from file extension: %s", ext)
-			viperFormat = strings.ToLower(ext)
-		} else {
-			log.Logger.Debug().Msg("No config file format passed and file has no extension, defaulting to YAML")
-			viperFormat = "yaml"
+		earlyLog("unmarshalling config into config struct")
+		if err := GlobalKoanf.UnmarshalWithConf("", nil, kUnmarshalConf); err != nil {
+			return fmt.Errorf("failed to unmarshal config from file %s: %w", path, err)
+		}
+		return nil
+	}
+	// Otherwise, we merge the config from the system and user config files.
+	earlyLog("no config file specified on command line, attempting to merge configs")
+
+	// Generate user config path: ~/.config/ochami/config.yaml
+	var userConfigFile string
+	user, err := user.Current()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: unable to fetch current user: %v\n", ProgName, err)
+		os.Exit(1)
+	}
+	userConfigFile = filepath.Join(user.HomeDir, ".config", "ochami", "config.yaml")
+
+	// Read config from each file in slice
+	type FileCfgMap struct {
+		File string
+		Cfg  Config
+	}
+	cfgsToCheck := []FileCfgMap{
+		FileCfgMap{File: "/etc/ochami/config.yaml"}, // System config
+		FileCfgMap{File: userConfigFile},            // User config
+	}
+	var cfgsLoaded []FileCfgMap
+	for _, cfg := range cfgsToCheck {
+		// Create koanf struct to load config from this file into
+		ko := koanf.NewWithConf(kConfig)
+
+		// Create config struct to unmarshal config from this file into
+		var c Config
+
+		// Copy global koanf unmarshal config, but unmarshal into config
+		// struct we made above
+		umc := kUnmarshalConf
+		umc.DecoderConfig.Result = &c
+
+		// Load config file into koanf struct
+		earlyLogf("attempting to load config file: %s", cfg.File)
+		err := ko.Load(file.Provider(cfg.File), configParser)
+		if errors.Is(err, os.ErrNotExist) {
+			earlyLogf("config file %s not found, skipping", cfg.File)
+			continue
+		} else if err != nil {
+			return fmt.Errorf("failed to load config file %s: %w", cfg.File, err)
+		}
+
+		// Unmarshal loaded config into local config struct to lint
+		// (i.e. check for unknown keys, etc).
+		if err := ko.UnmarshalWithConf("", nil, umc); err != nil {
+			return fmt.Errorf("failed to unmarshal config from %s: %w", cfg.File, err)
+		}
+
+		// Add local config struct to slice of loaded configs
+		cfg.Cfg = c
+		cfgsLoaded = append(cfgsLoaded, cfg)
+	}
+
+	// Merge loaded configs into global config. If none loaded, use default
+	// config (set above).
+	for _, cfgLoaded := range cfgsLoaded {
+		earlyLogf("merging in config from %s", cfgLoaded.File)
+		if err := GlobalKoanf.Load(structs.Provider(cfgLoaded.Cfg, "yaml"), nil, koanf.WithMergeFunc(mergeConfig)); err != nil {
+			return fmt.Errorf("failed to merge configs into global config: %w", err)
 		}
 	}
 
-	// Tell viper about config file
-	viper.SetConfigName(name)
-	viper.SetConfigType(viperFormat)
-	viper.AddConfigPath(base)
+	// Unmarshal merged config from Koanf into global config struct.
+	// koanf.UnMarshalWithConf won't unmarshal into the global config struct
+	// so we copy it, unmarhsl into the copy, then set the copy as the
+	// global config.
+	c := GlobalConfig
+	kuc := kUnmarshalConf
+	kuc.DecoderConfig.Result = &c
+	if err := GlobalKoanf.UnmarshalWithConf("", nil, kuc); err != nil {
+		return fmt.Errorf("failed to unmarshal global config into struct: %w", err)
+	}
+	GlobalConfig = c
 
-	// Load configuration from file
-	err := viper.ReadInConfig()
+	earlyLog("config files, if any, have been merged")
 
-	return err
+	return nil
 }
 
 // WriteConfig takes a path and config file format and writes the current viper
@@ -69,49 +207,34 @@ func LoadConfig(path, format string) error {
 // that viper accepts. If format is empty, the format is guessed by the config
 // file's file extension. If there is no file extension and format is empty,
 // YAML is used.
-func WriteConfig(path, format string) error {
+func WriteConfig(path string) error {
 	if path == "" {
 		return fmt.Errorf("no configuration file path passed")
 	}
-	base := filepath.Dir(path)
-	name := filepath.Base(path)
-	ext := strings.Trim(filepath.Ext(path), ".")
-	fullPath := filepath.Join(base, name)
-	log.Logger.Debug().Msgf("Configuration file is: %s", fullPath)
+	log.Logger.Debug().Msgf("writing config file: %s", path)
 
-	// Determine format of config file to tell viper to use.
-	var viperFormat string
-	if format != "" {
-		if ext != "" {
-			log.Logger.Debug().Msgf("Using passed value of %q as config file format even though file extension is %q", format, ext)
-		} else {
-			log.Logger.Debug().Msgf("Using passed value of %q as config file format", format)
-		}
-		viperFormat = strings.ToLower(format)
-	} else {
-		if ext != "" {
-			log.Logger.Debug().Msgf("No config file format passed, inferring from file extension: %s", ext)
-			viperFormat = strings.ToLower(ext)
-		} else {
-			log.Logger.Debug().Msg("No config file format passed and file has no extension, defaulting to YAML")
-			viperFormat = "yaml"
-		}
+	c, err := yaml.Marshal(GlobalConfig)
+	if err != nil {
+		return fmt.Errorf("failed to marshal config for writing: %w", err)
 	}
 
-	// Tell viper about config file
-	viper.SetConfigName(name)
-	viper.SetConfigType(viperFormat)
-	viper.AddConfigPath(base)
+	// Get mode if file exists
+	var fmode os.FileMode = 0o644
+	if finfo, err := os.Stat(path); err == nil {
+		fmode = finfo.Mode()
+	}
 
-	if err := viper.WriteConfig(); err != nil {
-		return fmt.Errorf("failed to write config file %s: %w", path, err)
+	// Write config file
+	if err := os.WriteFile(path, c, fmode); err != nil {
+		return fmt.Errorf("failed to write config to file %s: %w", path, err)
 	}
 	log.Logger.Info().Msgf("wrote config to %s", path)
 
 	return nil
 }
 
-func SetDefaults() {
-	viper.SetDefault("log.format", "json")
-	viper.SetDefault("log.level", "warning")
+func mergeConfig(src, dst map[string]interface{}) error {
+	// "name" is key used to identify each cluster config in config's
+	// cluster list.
+	return MergeMaps(src, dst, "name")
 }
