d226d376fd62950c5d7ac0542bb65af50b50b184
Devon Bautista <devonb@lanl.gov>
2025-03-04 21:53:49 -0700

    refactor: move config/log init to start of commands
    This is instead of doing it through cobra.OnInitialize. The main reason
    for this change is so that some commands (e.g. config show) don't ask
    the user to create nonexistent config files and err instead, wnile
    allowing others (e.g. config set) to do ask.

--- cmd/config-show.go
+++ cmd/config-show.go
@@ -22,10 +22,14 @@ var configShowCmd = &cobra.Command{
 		// To mark both persistent and regular flags mutually exclusive,
 		// this function must be run before the command is executed. It
 		// will not work in init(). This means that this needs to be
-		// presend in all child commands.
+		// present in all child commands.
 		cmd.MarkFlagsMutuallyExclusive("system", "user", "config")
 	},
 	Run: func(cmd *cobra.Command, args []string) {
+		// First and foremost, make sure config is loaded and logging
+		// works.
+		initConfigAndLogging(cmd, false)
+
 		// Get the config from the relevant file depending on the flag,
 		// or the merged config if none.
 		var cfg config.Config
