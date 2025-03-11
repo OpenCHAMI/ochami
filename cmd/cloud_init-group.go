c8d6360f2e4b575545e4649ec33bed95b9621da9
Devon Bautista <devonb@lanl.gov>
2025-03-11 14:35:03 -0600

    feat(cloud-init): add group command

---
+++ cmd/cloud_init-group.go
@@ -0,0 +1,26 @@
+// This source code is licensed under the license found in the LICENSE file at
+// the root directory of this source tree.
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// cloudInitGroupCmd represents the cloud-init-group command
+var cloudInitGroupCmd = &cobra.Command{
+	Use:   "group",
+	Args:  cobra.NoArgs,
+	Short: "Manage cloud-init groups",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			printUsageHandleError(cmd)
+			os.Exit(0)
+		}
+	},
+}
+
+func init() {
+	cloudInitCmd.AddCommand(cloudInitGroupCmd)
+}
